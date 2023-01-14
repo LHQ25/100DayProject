0bbb3478c51ad6110fa44b9bf39cc7867d87b199
louhanqing <louhanqing2012@163.com>
2023-01-14 19:10:02 +0800

    2023年01月14日19:09:59

---
+++ CarthageDemo/CarthageDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CarthageDemo
+//
+//  Created by 9527 on 2023/1/12.
+//
+
+import UIKit
+import Alamofire
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
